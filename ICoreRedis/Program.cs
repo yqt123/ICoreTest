4dbf70668fb9393fee799c247fd8a88295e5ed1b
yqt123 <864083509@qq.com>
2019-09-01 11:36:50 +0800

    11

---
+++ ICoreRedis/Program.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+
+namespace ICoreRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisManage.Set<string>("testkey", "123");
+
+            var val = RedisManage.Get<string>("testkey");
+
+            Console.WriteLine(val);
+        }
+    }
+}
---
+++ ICoreRedis/RedisManage.cs
@@ -0,0 +1,49 @@
+﻿using ServiceStack;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICoreRedis
+{
+    class RedisManage
+    {
+        public static PooledRedisClientManager ClientManager { get; private set; }
+        static RedisManage()
+        {
+            RedisClientManagerConfig redisConfig = new RedisClientManagerConfig();
+            redisConfig.MaxWritePoolSize = 128;
+            redisConfig.MaxReadPoolSize = 128;
+
+            //可以读写分离，指定一台服务器读，一台写。
+            // new PooledRedisClientManage(读写的服务器地址，只读的服务器地址
+            ClientManager = new PooledRedisClientManager(
+                new string[] { "redis123@47.112.114.44:6379" },
+                new string[] { "redis123@47.112.114.44:6379" }, redisConfig);
+        }
+
+        public static bool Set<T>(string key, T val)
+        {
+            using (IRedisClient con = RedisManage.ClientManager.GetClient())
+            {
+                return con.Set<T>(key, val);
+            }
+        }
+
+        public static T Get<T>(string key)
+        {
+            using (IRedisClient con = RedisManage.ClientManager.GetClient())
+            {
+                return con.Get<T>(key);
+            }
+        }
+
+        public static void AddItemToList(string key, string val)
+        {
+            using (IRedisClient con = RedisManage.ClientManager.GetClient())
+            {
+                con.AddItemToList(key, val);
+            }
+        }
+    }
+}
